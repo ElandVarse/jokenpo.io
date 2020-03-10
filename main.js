3fda4f848b04a6c78104fb19a227cde0280a7389
ElandVarse <andre.jobs23@gmail.com>
2020-03-10 13:36:26 -0300

    Update backend 3

--- main.js
+++ main.js
@@ -1,59 +1,111 @@
-// //Pensando em maneiras de diminuir e otimizar esse código
-// const texto = document.getElementById('texto');
+//Contador
+const contVit = document.querySelector('#vit')
+const contDer = document.querySelector('#der')
+const contEmp = document.querySelector('#emp')
+let victory=0;
+let lose=0;
+let draw=0;
+
 
 // //definindo resultado do computador
 const computerPlay = () => {
     const computer = Array.from(document.querySelectorAll('#right li img'));
-    const i = ( Math.floor(Math.random() * 3));
+    const i = (Math.floor(Math.random() * 3));
     const pcResult = computer[i];
-
-    pcResult.style.border='3px solid red'
-    return pcResult.id;
+    
+    computer.forEach(element => {
+        if(element===pcResult){
+            element.style.border='3px solid red'
+        }
+        else{
+            element.style.border='none'
+        }
+    })
+    return pcResult.id
 }
 
-document.getElementById('img').onclick= function(e){
-    //Contador contador 
-    let emp=0;
-    let vit=0;
-    let der=0;
 
-    //resultado do player
+document.querySelector('#left').onclick= function(e){
+    const text = document.querySelector('.middle .contour h3')
+    const resultados = document.querySelector('.middle .contour p')
+    
+    //resultado do player/pc
     const player = e.target.id;
-    const computer = computerPlay();    
-
-
-    const venceu = () => {
-        texto.innerHTML='Venceu';
-        resultados.innerHTML=`${player} bate ${computerPlay()}`;
-        vit+=1;
-        document.getElementById("vit").innerHTML=`Vitórias:${vit}`;
+    const computer = computerPlay();  
+    
+    const empate = () =>{
+        //Texto de resultado:
+        text.innerHTML='Empate!'
+        resultados.innerHTML=`${player} e ${computer}`
+        resultados.style.color='grey'
+        //Placar
+        draw+=1;
+        contEmp.innerHTML=`Empates:${draw}`
+        console.log('Empate!')
     }
-
-    const perdeu = () => {
-        console.log(`${computerPlay()} bate ${player}`)
+    const derrota = () =>{
+        text.innerHTML='Derrota!'
+        resultados.innerHTML=`${computer} bate ${player}`
+        resultados.style.color='red'
+        lose+=1;
+        contDer.innerHTML=`Derrotas:${lose}`
+        console.log('derrota!')
     }
-
-    const empate = () => {
-        console.log(`${computerPlay()} e ${player}`)
+    const vitoria = () =>{
+        text.innerHTML='Vitória!'
+        resultados.innerHTML=`${player} bate ${computer}`
+        resultados.style.color='green'
+        victory+=1;
+        contVit.innerHTML=`Vitórias:${victory}`
+        console.log('Vitória!')
     }
-
+    
     switch(player){
         case 'pedra':
             switch(computer){
-                case 'pcPe':
-                    console.log('Empate')
+                case 'pedra':
+                    empate();
                     break;
-                case 'pcPap':
-                    console.log('Perdeu')
+                case 'papel':
+                    derrota();
                     break;
-                case 'pcTes':
-                    venceu();
+                case 'tesoura':
+                    vitoria();
                     break;
             }
-    }
+            break;
 
-}
-        
+            case 'papel':
+                switch(computer){
+                    case 'pedra':
+                        vitoria();
+                        break;
+                    case 'papel':
+                        empate();
+                        break;
+                    case 'tesoura':
+                        derrota();
+                        break;
+                }
+                break;
+
+                case 'tesoura':
+                    switch(computer){
+                        case 'pedra':
+                            derrota();
+                            break;
+                        case 'papel':
+                            vitoria();
+                            break;
+                        case 'tesoura':
+                            empate();
+                            break;
+                    }
+                    break;
+                    default:
+                        console.log('Não entendi')
+        }
+    }   
 
     // switch(player){
     //     case 'pedra':
